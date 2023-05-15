a9ef82f833e50d2db1140b45a14f55c8bdb3bab2
Coder1804 <samsunga40uzb@gmail.com>
2023-05-15 10:07:28 +0500

    choose options fo'grlandi

--- src/components/common/ChooseOption.jsx
+++ src/components/common/ChooseOption.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 const ChooseOption = ({title,Icon, slogan , id}) => {
-    if(id === 2) return (
+    if(id === 1) return (
         <Link to="../price" className="flex items-center flex-1 flex-col gap-2 bg-white p-3 rounded-lg">
             <div className="text-text-dark-primary">
                 <Icon/>
